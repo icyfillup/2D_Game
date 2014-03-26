95cd7e2581e22169d55d7187df175eacdf8a0a98
icyfillup <phil10tran@gmail.com>
2014-03-26 15:50:24 -0700

    added a pop up window to the project

--- src/Game/Game.java
+++ src/Game/Game.java
@@ -1,11 +1,22 @@
 package Game;
 
-public class Game
+import javax.swing.JFrame;
+
+public class Game extends JFrame
 {
 
+	public Game(String name)
+	{
+		super(name);
+		setSize(1000, 800);
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setResizable(true);
+		setVisible(true);
+	}
+	
 	public static void main(String[] args)
 	{
-		
+		Game game = new Game("2D Game");
 
 	}
 
