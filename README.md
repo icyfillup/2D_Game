faca822a097a1f54a09394ae41890bb2e8a1e0a1
icyfillup <phil10tran@gmail.com>
2014-03-26 15:05:20 -0700

    created java project

---
+++ README.md
@@ -0,0 +1,7 @@
+# Goal
+Trying to make a 2D game
+
+- make the game functional
+- movement
+- collision detection 
+- ground 
---
+++ src/Game.java
@@ -0,0 +1,12 @@
+
+public class Game
+{
+
+	public static void main(String[] args)
+	{
+		
+		System.out.println("Lets get started");
+
+	}
+
+}
